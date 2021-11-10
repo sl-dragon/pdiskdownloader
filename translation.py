1b48027e200c10b21e6f2d53f121baa061143fb2
Harshit Shrivastav <87630299+Itzharshit@users.noreply.github.com>
2021-11-10 20:10:08 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -3,7 +3,7 @@ class Translation(object):
 I'm PYROGRAMMERS PDISK DOWNLOADER!
 You can download PDISK link, Using this bot!
 
-/help for more details!"""
+Hit /help for more details!"""
     FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> \nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons.\nYou can use /deletethumbnail to delete the auto-generated thumbnail."
     SET_CUSTOM_USERNAME_PASSWORD = """If you want to download premium videos, provide in the following format:
 URL | filename | username | password"""
